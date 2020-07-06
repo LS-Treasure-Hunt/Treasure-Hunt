7ac3dccb1e99021216bf417e10ccb1a235cc9b0d
Kevin Tou <toukevin@gmail.com>
2020-07-06 11:27:27 -0700

    Switched http to https

--- src/util/axiosWIthAuth.js
+++ src/util/axiosWIthAuth.js
@@ -7,7 +7,7 @@ export const axiosWithAuth = () => {
       Authorization: `Token ${process.env.REACT_APP_KEY}`,
       // Authorization: `Token ${process.env.REACT_APP_TEST_KEY}`
     },
-    baseURL: 'http://l-t-h.herokuapp.com/api/',
+    baseURL: 'https://l-t-h.herokuapp.com/api/',
     // baseURL: "http://127.0.0.1:8000/api/"
   });
 };
