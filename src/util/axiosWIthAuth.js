9c7d29a74ca0fbaba7781d573a0326aac1ca0700
May Ng <mngmay@gmail.com>
2020-08-09 18:12:39 -0700

    updated node-sass for version of node

--- src/util/axiosWIthAuth.js
+++ src/util/axiosWIthAuth.js
@@ -6,6 +6,7 @@ export const axiosWithAuth = () => {
       "Content-Type": "application/json",
       Authorization: `Token ${process.env.REACT_APP_KEY}`
       // Authorization: `Token ${process.env.REACT_APP_TEST_KEY}`
+      // Authorization: `Token ${process.env.REACT_APP_NO_ABILITY_KEY}`
     },
     baseURL: "https://lambda-treasure-hunt.herokuapp.com/api/"
     // baseURL: "http://127.0.0.1:8000/api/"
