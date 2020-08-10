a8307ddc195c3965b0bc49034ed02b7a6a124374
May Ng <mngmay@gmail.com>
2020-08-10 09:38:18 -0700

    cleaned up axiosWithAuth

--- src/util/axiosWIthAuth.js
+++ src/util/axiosWIthAuth.js
@@ -1,14 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const axiosWithAuth = () => {
   return axios.create({
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
       Authorization: `Token ${process.env.REACT_APP_KEY}`,
-      // Authorization: `Token ${process.env.REACT_APP_TEST_KEY}`
-      // Authorization: `Token ${process.env.REACT_APP_NO_ABILITY_KEY}`
     },
-    baseURL: 'https://l-t-h.herokuapp.com/api/',
-    // baseURL: "http://127.0.0.1:8000/api/"
+    baseURL: "https://l-t-h.herokuapp.com/api/",
   });
 };
